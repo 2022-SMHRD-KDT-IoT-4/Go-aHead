3451c0783c7dc61d5a7cf2b5434911bb8cc545cf
SMHRD <SMHRD@DESKTOP-JLCAU0V>
2022-06-02 17:18:06 +0900

    -commit

--- Go_aHead/src/main/java/synchrfo/Test.java
+++ Go_aHead/src/main/java/synchrfo/Test.java
@@ -1,5 +1,7 @@
 package synchrfo;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Test {
 	// test중입니다.
 	// 원표씨 컴퓨터에서 수정중
@@ -10,9 +12,7 @@ public class Test {
 		System.out.println("원표씨 컴퓨터~");
 		String name = "홍" + "원표";
 		System.out.println("수정한사람 " + name);
-		System.out.println("열심히 수정중입니다.");
-
-		System.out.print("dasdasdasdas");
-		System.out.print("dasds");
+		System.out.println("메서드 만들기");
+		System.out.println("test");
 	}
 }
