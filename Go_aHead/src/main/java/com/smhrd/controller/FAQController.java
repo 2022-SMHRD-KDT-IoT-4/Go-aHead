c0ddc93c6f836801d304780ffe2e3a547cdbea04
SMHRD <SMHRD@DESKTOP-R3HS2U9>
2022-06-08 14:49:08 +0900

    희원 고객센터 완료

--- Go_aHead/src/main/java/com/smhrd/controller/FAQController.java
+++ Go_aHead/src/main/java/com/smhrd/controller/FAQController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ViewFAQController implements Controller {
+public class FAQController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
