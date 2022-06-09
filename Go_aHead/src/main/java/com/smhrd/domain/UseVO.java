42ae3f74366bbac9c3ab6a4f62c371474341f24e
SMHRD <SMHRD@DESKTOP-JLCAU0V>
2022-06-09 09:21:42 +0900

    제발

---
+++ Go_aHead/src/main/java/com/smhrd/domain/UseVO.java
@@ -0,0 +1,16 @@
+package com.smhrd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UseVO {
+	
+	private String kick_num;
+	private String hel_num;
+	private String mem_id;
+	
+}
