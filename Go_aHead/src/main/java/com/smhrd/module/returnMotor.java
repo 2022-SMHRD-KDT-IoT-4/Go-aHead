881560b2b15cb7c76575c8ad52f34a805b2d9dd1
SMHRD <SMHRD@DESKTOP-TPHOIKK>
2022-06-11 15:30:08 +0900

    returnmotor hj

--- Go_aHead/src/main/java/com/smhrd/module/returnMotor.java
+++ Go_aHead/src/main/java/com/smhrd/module/returnMotor.java
@@ -15,29 +15,33 @@ import com.smhrd.domain.moduleVO;
 @SuppressWarnings("serial")
 @WebServlet("/returnMotor")
 public class returnMotor extends HttpServlet {
-	static HttpServletRequest requset;
-	public static int alcohol = 0;
-	public static moduleVO vo = (moduleVO)requset.getAttribute("motorVO"); 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int motor = vo.getMotor();
-		int press = module.press;
-		
-		if(request.getParameter("alcohol") != null) {
-			alcohol = Integer.parseInt(request.getParameter("alcohol"));
-//		    System.out.println(alcohol);
-		}
-//		System.out.println("press값 :" + module.press);
-		PrintWriter out = response.getWriter();
-		
-		if (press > 10 && motor==1) {
-			out.println("1");
-			System.out.println("presson");
-		} if(motor == 2) {
-			out.println("2");
-			System.out.println("motorStop");
-		} else {
-			out.println("0");
-			System.out.println("nononono");
-		}
-	}
+   static HttpServletRequest requset;
+   public static int alcohol = 0;
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      HttpSession session = request.getSession();
+      int motor = 0;
+      if (session.getAttribute("motorState") != null) {
+         motor = Integer.parseInt((String)session.getAttribute("motorState"));
+      }
+      
+      int press = module.press;
+      
+      if(request.getParameter("alcohol") != null) {
+         alcohol = Integer.parseInt(request.getParameter("alcohol"));
+//          System.out.println(alcohol);
+      }
+//      System.out.println("press값 :" + module.press);
+      PrintWriter out = response.getWriter();
+      
+      if (press > 10 && motor==1) {
+         out.println("1");
+         System.out.println("presson");
+      } if(motor == 2) {
+         out.println("2");
+         System.out.println("motorStop");
+      } else {
+         out.println("0");
+         System.out.println("nononono");
+      }
+   }
 }
